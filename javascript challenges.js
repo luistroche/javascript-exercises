d8d018d321d65b34c36f629e9e0cecb75807ad33
Luis Troche <luistroche@gmail.com>
2017-09-18 21:34:24 -0400

    finished falsy bouncer

--- javascript challenges.js
+++ javascript challenges.js
@@ -374,3 +374,25 @@ function mutation(palabras){
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Falsy Bouncer
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// Here are some helpful links:
+
+// Boolean Objects
+// Array.prototype.filter()
+
+function bouncer(arr){
+  var filtrado = arr.filter(function(a){
+    return Boolean(a) == true;
+  });
+  return (filtrado);
+}
+
+
