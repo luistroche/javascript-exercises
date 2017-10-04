b8139830b57a4da9e6e3ff7f530777687153b0b8
Luis Troche <luistroche@gmail.com>
2017-10-03 20:17:23 -0400

    added caesas cipher

--- javascript challenges.js
+++ javascript challenges.js
@@ -487,3 +487,53 @@ function getIndexToIns(arr, num){
 arreglador([3, 10, 5], 3);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Caesars Cipher 
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// Here are some helpful links:
+
+// String.prototype.charCodeAt()
+// String.fromCharCode()
+
+
+function rot13(str){
+  var frase ="";
+  var e = 0;
+  
+  while(e < str.length){
+    if(str.charCodeAt(e)< 65){
+      frase = frase + String.fromCharCode(str.charCodeAt(e));
+      e++;
+    }else if(str.charCodeAt(e) <= 77){
+
+      frase = frase + String.fromCharCode(str.charCodeAt(e)+13);
+      e++;
+    }else if(str.charCodeAt(e) <= 90){
+
+      frase = frase + String.fromCharCode(str.charCodeAt(e)-13);
+      e++;
+    }else{
+
+      frase = frase + String.fromCharCode(str.charCodeAt(e));
+      e++;
+    }
+    
+    
+
+  }//end while
+  return frase;
+}//end traductor function
+
+
+// Change the inputs below to test
+// rot13("SERR PBQR PNZC");
+
